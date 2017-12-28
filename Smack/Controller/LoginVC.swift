c958654f1d62a6ad16811efb017bfcb218aa079d
Maike Warner <MaikeWarner@Maikes-MacBook-Air.local>
2017-12-28 20:55:50 +0100

    Lesson 4.

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Maike Warner on 12/28/17.
+//  Copyright © 2017 Maike. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  @IBAction func closePressed(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
+  }
+  
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Maike Warner on 12/28/17.
+//  Copyright © 2017 Maike. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+//Generally they use caps for constants
+let TO_LOGIN = "toLogin"
+
+
+
